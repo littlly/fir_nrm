fd52a89a34ac06df7715a0c877306446594f0549
ricam <rmf20@mails.tsinghua.edu.cn>
2023-03-23 21:40:28 +0800

    we use trendline in the model to down the loss and it looks useful!

--- lib/trendLayer.py
+++ lib/trendLayer.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class TrendNormalize(nn.Module):
-    def __init__(self):
+    def __init__(self, dim=-1):
         super(TrendNormalize, self).__init__()
+        self.dim = dim
 
     def forward(self, x):
         """
@@ -12,19 +13,19 @@ class TrendNormalize(nn.Module):
         :return: 趋势化后的数据，PyTorch张量
         """
         # 计算数据的均值和标准差
-        mean = torch.mean(x, dim=0, keepdim=True)
-        std = torch.std(x, dim=0, keepdim=True)
+        mean = torch.mean(x, dim=self.dim, keepdim=True)
+        std = torch.std(x, dim=self.dim, keepdim=True)
         # 对数据进行标准化
         x = (x - mean) / std
         # 计算数据的趋势
         trend = torch.zeros_like(x)
-        trend[1:] = x[1:] - x[:-1]
+        trend[...,1:] = x[...,1:] - x[...,:-1]
         # 对趋势进行标准化
-        trend_mean = torch.mean(trend, dim=0, keepdim=True)
-        trend_std = torch.std(trend, dim=0, keepdim=True)
+        trend_mean = torch.mean(trend, dim=self.dim, keepdim=True)
+        trend_std = torch.std(trend, dim=self.dim, keepdim=True)
         trend = (trend - trend_mean) / trend_std
         # 将趋势化后的数据和趋势合并
         data_trend = torch.zeros_like(x)
-        data_trend[0] = x[0]
-        data_trend[1:] = data_trend[:-1] + trend
+        data_trend[...,0] = x[...,0]
+        data_trend[...,1:] = data_trend[...,:-1] + trend[...,:-1]
         return data_trend
